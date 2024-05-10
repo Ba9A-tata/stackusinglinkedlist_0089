5e1db12f0be98a81de911b731704576385620b47
Ba9A-tata <yogadzakkii@gmail.com>
2024-05-10 14:39:24 +0700

    menambahkan case 1

--- stackusinglinkedlist_0089.cpp
+++ stackusinglinkedlist_0089.cpp
@@ -66,7 +66,10 @@ int main(){
         cin >> choice;
 
         switch (choice){
-            
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value); // Push the entered value onto the stack
         }
 
     }
