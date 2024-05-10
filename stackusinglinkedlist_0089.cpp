1f075ecb867f19e1708e1b8e707be706f09fa2d6
Ba9A-tata <yogadzakkii@gmail.com>
2024-05-10 14:27:48 +0700

    membuat void peek

--- stackusinglinkedlist_0089.cpp
+++ stackusinglinkedlist_0089.cpp
@@ -18,6 +18,22 @@ private:
     Node *top; // Pointer to the top of the stack
 
 public:
-    
+    Stack(){
+        top = NULL; // Initialize the stack with a null top pointer
+    }
+    // Push operation Inset an element onto the top of the stack
+    int push(int value){
+        Node* newNode = new Node(); // 1. Allocate memory of the new node
+        newNode->data = value; // 2. assign value
+        newNode->next = top; // 3. Set the next pointer of the new node to the current top node
+        top = newNode; // 4. Update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+    // Peek/Top operation: Retrieve the value of the topmost element without removing it
+    void peek(){
+        
+    }
 }
 
